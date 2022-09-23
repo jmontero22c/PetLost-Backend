0e0034958bc4fae67c2ec46c5da70cfc91f7a90d
OnfeVS <jorgevallejo2990@gmail.com>
2022-09-22 22:26:13 -0500

    update
    Actualizar contrasenia en properties.

--- src/main/java/com/petlost/petlost/Controllers/MascotaController.java
+++ src/main/java/com/petlost/petlost/Controllers/MascotaController.java
@@ -25,41 +25,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-
 @RestController
 public class MascotaController {
     @Autowired
     MascotaDao mascotaDao;
-    
-    @RequestMapping(value="mascotas")
-    public List<Mascota> getPet(){
+
+    @RequestMapping(value = "mascotas")
+    public List<Mascota> getPet() {
         return mascotaDao.getPet();
     }
-    
-    @RequestMapping(value="mascotas/add", method=RequestMethod.POST)
-    public String createPet(@RequestBody Mascota pet/*, @RequestParam(defaultValue = "") MultipartFile image */){
-        /*if(!image.isEmpty()){
-            Path imageDirectory = Paths.get("http://127.0.0.1:5500/assets/img");
-            String pathAbsolute = imageDirectory.toFile().getAbsolutePath();
 
-            try {
-                byte[] bytesImg = image.getBytes();    
-                Path fullPath = Paths.get(pathAbsolute + "/" + image.getOriginalFilename());
-                Files.write(fullPath, bytesImg);
-
-                pet.setPhoto(image.getOriginalFilename());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            
-        }*/
+    @RequestMapping(value = "mascotas/add", method = RequestMethod.POST)
+    public String createPet(@RequestBody Mascota pet/*
+                                                     * ,
+                                                     * 
+                                                     * @RequestParam(defaultValue = "") MultipartFile image
+                                                     */) {
+        /*
+         * if(!image.isEmpty()){
+         * Path imageDirectory = Paths.get("http://127.0.0.1:5500/assets/img");
+         * String pathAbsolute = imageDirectory.toFile().getAbsolutePath();
+         * 
+         * try {
+         * byte[] bytesImg = image.getBytes();
+         * Path fullPath = Paths.get(pathAbsolute + "/" + image.getOriginalFilename());
+         * Files.write(fullPath, bytesImg);
+         * 
+         * pet.setPhoto(image.getOriginalFilename());
+         * } catch (IOException e) {
+         * e.printStackTrace();
+         * }
+         * 
+         * }
+         */
 
         return mascotaDao.createPet(pet);
     }
 
     @Transactional
-    @RequestMapping(value="mascotas/del/{id}", method = RequestMethod.DELETE)
-    public String deletePet(@PathVariable(value="id") Long idPet){
+    @RequestMapping(value = "mascotas/del/{id}", method = RequestMethod.DELETE)
+    public String deletePet(@PathVariable(value = "id") Long idPet) {
         return mascotaDao.deletePet(idPet);
     }
 }
